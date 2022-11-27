e6d2d8af7141102056bdf38d62aa973e19f1e300
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-11-27 18:28:52 -0300

    :tada: add: list of alumnos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lista-alumnos';
+  public alumnos = [
+    {
+      name: 'Juan',
+      promedio: 8.5,
+      top: false,
+    },
+    {
+      name: 'Sofía',
+      promedio: 9,
+      top: false,
+    },
+    {
+      name: 'Mariano',
+      promedio: 10,
+      top: true,
+    },
+    {
+      name: 'Juliana',
+      promedio: 6.5,
+      top: false,
+    },
+    {
+      name: 'Mateo',
+      promedio: 8,
+      top: false,
+    },
+  ];
 }
